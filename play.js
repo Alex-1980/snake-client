d23d39f81e0713a513ed11edf462935fc534d491
Chansoo Han <devhan1207@gmail.com>
2023-04-12 10:23:42 -0400

    imported from client.js

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
+const {connect} = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here
-    port: 3000 // PORT number here,
-  });
-
-  conn.on("data", (data) => {
-    console.log("from server: ", data);
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
